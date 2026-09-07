14d5bd91fdc8e549e2f0b01b83ed7108449fbe76
macbook <macbook@imabook.local>
2026-09-08 01:09:52 +0300

    Clean submission files

--- database/create-editor-user.sql
+++
@@ -1,4 +0,0 @@
--- Run this script as a MySQL administrator only for local development.
-CREATE USER IF NOT EXISTS 'editor'@'localhost' IDENTIFIED BY '12345678';
-GRANT SELECT, INSERT, UPDATE, DELETE ON ProductsApplication.* TO 'editor'@'localhost';
-FLUSH PRIVILEGES;
